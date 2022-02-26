55bf9a381a4d69a78a861400821de98b7bc0eead
danicaliforrnia <d1as2n3i4e5l6>
2022-02-26 11:01:50 -0400

    feat: create constants directory
    - menus file contains topbar menus.
    - styles file contains constant heights and widths.

---
+++ movies-app/src/constants/menus.js
@@ -0,0 +1,14 @@
+const MENUS = [
+    {
+        id: 1,
+        text: 'Movies',
+        route: '/'
+    },
+    {
+        id: 2,
+        text: 'Transfers',
+        route: '/transfers'
+    }
+]
+
+export default MENUS;
