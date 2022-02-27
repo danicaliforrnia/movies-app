23f2103a619c5023de2562158264ad16292efd64
danicaliforrnia <d1as2n3i4e5l6>
2022-02-27 14:41:24 -0400

    feat: implement bunya logger

--- api/server.mjs
+++ api/server.mjs
@@ -4,8 +4,17 @@ import { config } from 'dotenv';
 import { moviesRoute } from './src/routes/movies.route.mjs';
 import { studiosRoute } from './src/routes/studios.route.mjs';
 import { disney, sony, warner } from './constants/studio_constants.mjs';
+import { v4 } from 'uuid';
+import pkg from 'bunyan';
+
+const { createLogger, stdSerializers } = pkg;
 
 config();
+
+const log = createLogger({
+    name: 'my-app',
+    serializers: stdSerializers
+});
 
 const port = process.env.PORT || 8080;
 
@@ -16,6 +25,12 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+    req.log = log.child({ req_id: v4() }, true);
+    req.log.info({ req });
+    res.on('finish', () => req.log.info({ res }));
+    next();
+});
 app.use(`${ process.env.BASE_URL }/movies`, moviesRoute);
 app.use(`${ process.env.BASE_URL }/studios`, studiosRoute);
 app.get('/api/v2/health', (req, res) => res.json({ status: 'UP' }));
